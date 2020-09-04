9836f72e47da4150713deaf2b48d3bc2d70decb7
jplafata <jplafata@gmail.com>
2020-09-04 14:56:53 -0400

    Uploading boostrap theme and child theme

---
+++ Themes/wp-bootstrap-starter-child/js/image-hover-block.js
@@ -0,0 +1,17 @@
+registerBlockType('heyjp,custom-image-hover',{
+  //Built-in attributes
+  title: 'Hover Text Image',
+  description: 'Displays an image with text overlay and hover effect',
+  icon: 'format-image',
+  category: 'layout',
+
+  //custom attributes
+  attributes:{},
+  //custom functions
+
+  //built-in functions
+  edit(){},
+
+  save(){}
+
+});
