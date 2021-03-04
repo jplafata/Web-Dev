ddf0aaeb13058aa49d09253e5c5274bd3fdd919a
John-Paul <jplafata@gmail.com>
2021-03-03 21:18:17 -0500

    got cover page working!

--- Themes/wp-bootstrap-starter-child/js/child-script.js
+++ Themes/wp-bootstrap-starter-child/js/child-script.js
@@ -51,7 +51,6 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
 })(jQuery); // End of use strict
 
  jQuery( function ( $ ) {
-   console.log("in the new script!");
    $( ' .widget_nav_menu ul' ).removeClass( 'flex-column' );
    $( '#footer-widget section').wrap( '<div class="col-12 col-md-4 text-center"></div>');
  });
